3b07f548e7eef6fece43800904af26e9a4fc763b
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-23 00:50:02 +0000

    Added emojis

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🔴 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Lighthouse Labs.");
+assertEqual(100, 100);
+assertEqual(101, 100);
