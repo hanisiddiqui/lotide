be4c7fcf5d67d3d5ea619284b802f4f41a741546
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-24 05:11:14 +0000

    completed letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const indexPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+
+  for (let index in sentence) {
+    if (sentence[index] !== ' ')
+    {
+      if (results[sentence[index]]) {
+        results[sentence[index]].push(Number(index));
+      }
+      else {
+        results[sentence[index]] = [Number(index)];
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  let bool = eqArrays(array1, array2);
+  if (bool) {
+    console.log('the same');
+  }
+  else {
+    console.log('different');
+  }
+};
+
+assertArraysEqual(indexPositions('hello').e, [1]);
