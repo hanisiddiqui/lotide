2fb67c7b6442ee8c5f4fe35e0479ec2a95369e53
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-24 06:23:55 +0000

    completed findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (input, show) {
+  let result;
+
+  for (let key in input) {
+    if (show === input[key]) {
+      result = key;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
