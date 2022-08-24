07d6f4428366ccfdecd2f4169dc8bc85482a52a8
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-24 04:54:09 +0000

    Completed countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const count = {};
+
+  for (let letter of sentence) {
+    if (letter != ' ')
+    {
+      if (count[letter]) {
+        count[letter]++;
+      }
+      else {
+        count[letter] = 1;
+      }
+    }
+  }
+  console.log(count);
+};
+
+countLetters("lighthouse in the house")
