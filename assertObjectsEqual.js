ebab80e1914b7dfaa340f6b7d268e789f7a8d32a
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-24 10:53:00 +0000

    completed assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,49 @@
+const eqObjects = function (object1, object2) {
+  let result;
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (let key of Object.keys(object1)) {
+    if (object1[key].length !== object2[key].length) {
+      return false;
+    }
+    else if (Array.isArray(object1[key]))
+    {
+      result = eqArrays(object1[key], object2[key]);
+      if (result === false) {
+        return result;
+      }
+    }
+    else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log('false');
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+
+  let bool = eqObjects(actual, expected);
+
+  if (bool === false) {
+    console.log('different');
+  }
+  else {
+    console.log('the same');
+  }
+};
+
+assertObjectsEqual({'a':1,'b':[1,2,3]}, {'b':[1,2,3], 'a':1});
+assertObjectsEqual({'a':'1','b':[1,2,3]}, {'b':[1,2,3], 'a':1});
