c7610a5b053e41620c6641fcda8bd77fe2f811bb
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-25 23:14:31 +0000

    completed map.js

---
+++ map.js
@@ -0,0 +1,37 @@
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+    console.log(callback(item));
+  }
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  let bool = eqArrays(array1, array2);
+  if (bool) {
+    console.log('the same');
+  }
+  else {
+    console.log('different');
+  }
+};
+
+const firstLetter = word => word[0];
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words1 = ["Ground", "12345678", "to", "major", "tom"];
+const words2 = ["ground", 1234567, "to", "major", "tom"];
+
+assertArraysEqual(map(words, firstLetter), ['g','c','t','m','t']);
+assertArraysEqual(map(words1, firstLetter), ['G','1','t','m','t']);
+assertArraysEqual(map(words2, firstLetter), ['g',1,'t','m','t']);
