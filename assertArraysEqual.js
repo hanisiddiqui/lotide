c68f5982a14457a186fcdb0fb3c0684557db8f75
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-08-30 02:01:58 +0000

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (array1, array2) {
   let bool = eqArrays(array1, array2);
@@ -17,5 +10,4 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, '2', 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
